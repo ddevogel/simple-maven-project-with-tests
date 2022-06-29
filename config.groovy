5e559caad0f0283e3a2dd6650dd54f8d4954e426
ddevogel <ddevogel@yahoo.com>
2022-06-29 15:22:27 -0500

    Update config.groovy

--- config.groovy
+++ config.groovy
@@ -130,6 +130,7 @@ class Config {
 
 def getFileContent(environment, text) {
     json = new JsonSlurperClassic().parseText(text)
+    println(json["common"])
     config = new Config(json["common"]).Extend(json[environment.toLowerCase()])
     //File file = new File("./out.txt")
 }
