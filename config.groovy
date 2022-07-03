3b839614a0dc371f6e2a0612e15a25976d6b2925
ddevogel <ddevogel@yahoo.com>
2022-07-02 22:42:03 -0500

    Update config.groovy

--- config.groovy
+++ config.groovy
@@ -10,11 +10,11 @@ class Db {
     Db(json) {
         this.set(json)
     }
-    @NonCPS
+    //@NonCPS
     def extend(json) {
         this.set(json)
     }
-    @NonCPS
+    //@NonCPS
     def private set(json) {
         if(json == null) return
         this.host = json.host ?: this.host
@@ -23,7 +23,7 @@ class Db {
         this.password = json.password ?: this.password
         this.sslmode  = json.sslmode ?: this.sslmode
     }
-    @NonCPS
+    //@NonCPS
     def String toString() {
         return String.format("DATABASE_CONNECTION='host=%s dbname=%s user=%s password=%s sslmode=%s'", 
          this.host,
@@ -46,11 +46,11 @@ class Queue {
     Queue(json) {
         this.set(json)
     }
-    @NonCPS
+    //@NonCPS
     def extend(json) {
         this.set(json)
     }
-    @NonCPS
+    //@NonCPS
     def private set(json) {
         if(json == null) return
         this.host = json.host ?: this.host
@@ -61,7 +61,7 @@ class Queue {
         this.password = json.password ?: this.password
         this.no_ack = (json.no_ack != null) ? json.no_ack : this.no_ack
     }
-    @NonCPS
+    //@NonCPS
     def String toString() {
         return String.format("QUEUE_HOST=%s\nQUEUE_PORT=%d\nQUEUE_EXCH=%s\nQUEUE_QUEUE=%s\nQUEUE_USER=%s\nQUEUE_PWD=%s\nQUEUE_NO_ACK=%d",
          this.host,
@@ -81,17 +81,17 @@ class NukeSvc {
     NukeSvc(json) {
         this.set(json)
     }
-    @NonCPS
+    //@NonCPS
     def extend(json) {
         this.set(json)
     }
-    @NonCPS
+    //@NonCPS
     def private set(json) {
         if(json == null) return        
         this.url = json.url ?: this.url
         this.secret = json.secret ?: this.secret
     }
-    @NonCPS
+    //@NonCPS
     def String toString() {
         return String.format("NUKE_SVC_URL=%s\nNUKE_SVC_KEY=%s",
          this.url,
@@ -106,17 +106,17 @@ class Chargebee {
     Chargebee(json) {
         this.set(json)
     }
-    @NonCPS
+    //@NonCPS
     def extend(json) {
         this.set(json)
     }
-    @NonCPS
+    //@NonCPS
     def private set(json) {
         if(json == null) return        
         this.site = json.site ?: this.site
         this.api_key = json.api_key ?: this.api_key
     }
-    @NonCPS
+    //@NonCPS
     def String toString() {
         return String.format('CHARGEBEE_SITE=%s\nCHARGEBEE_FULL_ACCESS_KEY=%s',
          this.site,
@@ -125,36 +125,38 @@ class Chargebee {
     }
 }
 class Config {
-    Db database
-    Queue queue
-    NukeSvc nuke
-    Chargebee chargebee
+    def String[] hosts
+    def Db database
+    def Queue queue
+    def NukeSvc nuke
+    def Chargebee chargebee
+    def Object secrets
 
-    Config(Object json) {
-        //if (json == null) return null
-        if (this.database == null) {
-            this.database = new Db(json.database)
-        } else {
-            this.database.extend(json.database)
-        }
-        if (this.queue == null) {
-            this.queue = new Queue(json.queue)
-        } else {
-            this.queue.extend(json.queue)
-        }
-        if (this.nuke == null) {
-            this.nuke = new NukeSvc(json.nuke_svc)
-        } else {
-            this.nuke.extend(json.nuke_svc)
-        }        
-        if (this.chargebee == null) {
-            this.chargebee = new Chargebee(json.chargebee)
-        } else {
-            this.chargebee.extend(json.chargebee)     
-        }
+    Config(Object json, secrets) {
+        this.secrets = secrets
+        this.set(json)
     }
-    @NonCPS
+    //@NonCPS
     def Config extend(json) {
+        this.set(json)
+        return this        
+    }
+    def String getFileContent() {
+        return String.format("%s\n%s\n%s\n%s",
+            this.database.toString(),
+            this.queue.toString(),
+            this.nuke.toString(),
+            this.chargebee.toString()              
+        )        
+        .replace("**db_pwd**", this.secrets.db_pwd)
+        .replace("**rmq_pwd**", this.secrets.rmq_pwd) 
+        .replace("**nuke_svc_key**", this.secrets.nuke_svc_key) 
+        .replace("**chargebee_key**", this.secrets.chargebee_key) 
+    }
+    def private set(json) {
+        if (json.hosts != null) {
+            this.hosts = json.hosts
+        } 
         if (this.database == null) {
             this.database = new Db(json.database)
         } else {
@@ -175,59 +177,33 @@ class Config {
         } else {
             this.chargebee.extend(json.chargebee)
         }
-        return this        
     }
-    @NonCPS
-    def String toString() {
-        return String.format("%s\n%s\n%s\n%s",
-            this.database.toString(),
-            this.queue.toString(),
-            this.nuke.toString(),
-            this.chargebee.toString()              
-          )
-    }
+    //@NonCPS
+
 }
 
-// @NonCPS
-// static def convertLazyMapToLinkedHashMap(def value) {
-//     if (value instanceof LazyMap) {
-//       Map copy = [:]
-//       for (pair in (value as LazyMap)) {
-//         copy[pair.key] = convertLazyMapToLinkedHashMap(pair.value)
-//       }
-//       println("here")
-//       return copy
-//     } else {
-//       return value
-//     }
-// }
-
-@NonCPS
+//@NonCPS
 def parseJsonText(String json) {
     def slurper = new JsonSlurper()
-    def parsed = new HashMap<>(slurper.parseText(json))
-    return parsed
+    def result = new HashMap<>(slurper.parseText(json))    
+    return result
 }  
 
-@NonCPS
-def String build(environment, text, secrets) {
-    def json = parseJsonText(text)
-    def _secrets = parseJsonText(secrets)
-    println(_secrets)
-    def config = new Config(json["common"]).extend(json[environment.toLowerCase()]).toString()
-        .replace("**db_pwd**", _secrets.db_pwd)
-        .replace("**rmq_pwd**", _secrets.rmq_pwd) 
-        .replace("**nuke_svc_key**", _secrets.nuke_svc_key) 
-        .replace("**chargebee_key**", _secrets.chargebee_key) 
+//@NonCPS
+def build(environment, text, secrets) {
+    def jsonText = parseJsonText(text)
+    def jsonSecrets = parseJsonText(secrets)
+    def config = new Config(jsonText["common"], jsonSecrets)
+        .extend(jsonText[environment.toLowerCase()])
 
-    return config.toString()
+    return config
 }
 
 return this
 
 /*
 To debug
-    - comment out @NonCPS everywhere
+    - comment out //@NonCPS everywhere
     - comment out last "return this" above
     - uncomment debug code below
 */
@@ -238,4 +214,6 @@ To debug
 // "nuke_svc_key":"NUKEKEY",
 // "chargebee_key":"CBKEY"
 // }"""
-// println(build("Dev", text, secrets))
+// config = build("Dev", text, secrets)
+// println(config.hosts)
+// println(config.getFileContent())
